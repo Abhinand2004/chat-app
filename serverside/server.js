c297a71e4193ac93f205a687993a18d6b25bd0a0
abhi <abhinandc293@gmail.com>
2025-02-24 15:11:52 +0530

    origin

--- serverside/server.js
+++ serverside/server.js
@@ -20,7 +20,7 @@ const io = new Server(httpServer, {
   }
 });
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({  origin: ["http://localhost:5173", "https://chat-app-puce-six.vercel.app"], credentials: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(passport.initialize()); 
 
